5882e4f647a168f87ab01012c25c2d8b1c9d796f
adrianhuebner <adrianh92@gmail.com>
2019-07-09 16:10:43 -0700

    rewording questions to make it so user knows how to respond to questions

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // asking the user for their name prompt
 var UserName = prompt('Hi! My name is Adrian! What`s your name?');
 alert('Hello ' + UserName + '! It\s nice to meet you! We are going to see how well you know me with a guessing game!');
-console.log('Learning the name of ' + UserName + ' who, is going to play a game about me!');
+console.log('Learning the name of ' + UserName + ' who, is going to play a game about me! Please, answer with a yes or a no to each question.');
 
 // asking the user if they knew where I grew up
 var grewUp = prompt('Do you think that I grew up in the same house that my father grew up in?');
@@ -39,13 +39,13 @@ if (typeEducation.toLowerCase() === 'yes') {
 console.log('Asking ' + UserName + ' about if I attened private or public schooling.')
 
 // asking the user who caused my love of reading
-var dadReads = prompt('Do you think that it was my dad who helped me begin to love to read?')
+var dadReads = prompt('Do you think that it was my mom who helped me begin to love to read?')
 
 // creating an if/else statement about my love of reading
-if (dadReads.toLowerCase() === 'yes') {
-  alert('Yes, my dad made me love to read.');
+if (dadReads.toLowerCase() === 'no') {
+  alert('It was actually my dad who got me to love reading!');
 } else{
-  alert('Actually, ' + UserName + ' my dad create my love of reading.');
+  alert('Actually, ' + UserName + ' it was my dad create my love of reading.');
 }
 console.log('Asking ' + UserName + ' whether or not it was my dad who made me love to read.');
 
