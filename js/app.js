343445b1464c2efd9684aeba02edc9ebd57e3f87
adrianhuebner <adrianh92@gmail.com>
2019-07-09 14:56:02 -0700

    adding third question about my fathers role in my love of reading

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ console.log('Learning the name of ' + UserName + ' who, is going to play a game 
 var grewUp = prompt('Do you think that I grew up in the same house that my father grew up in?');
 
 // creating and if/else statement about where I grew up
-if (grewUp === 'yes') {
+if (grewUp.toLowerCase() === 'yes') {
   alert('Yes! I did grow up in the same house as my father!');
 } else {
   alert('Actually, I did grow up in the same house that my father grew up in!');
@@ -19,10 +19,27 @@ console.log('My first question is asking ' + UserName + ' if they knew that I gr
 // asking the user if they know how many siblings I have
 var siblingAmmount = prompt('Do I have more than one sibling?');
 
-//creating an if/else statement about my sister
-if (siblingAmmount === 'no') {
+// creating an if/else statement about my sister
+if (siblingAmmount.toLowerCase() === 'no') {
   alert('That\s right, I only have one sister!');
 }else {
   alert('Actually ' + UserName + ' I only have one younger sister.');
 }
 console.log('Asking ' + UserName + 'if they know how many siblings that I have.');
+
+// asking the user about where I went to school
+var typeEducation = prompt('Did I go to private school from Preschool to High School?')
+
+//creating an if/else statement about school
+
+
+// asking the user who caused my love of reading
+var dadReads = prompt('Do you think that it was my dad who helped me begin to love to read?')
+
+// creating an if/else statement about my love of reading
+if (dadReads.toLowerCase() === 'yes') {
+  alert('Yes, my dad made me love to read.');
+} else{
+  alert('Actually, ' + UserName + ' my dad create my love of reading.');
+}
+console.log('Asking ' + UserName + ' whether or not it was my dad who made me love to read.');
